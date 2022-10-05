6d1e773d02d59163f2024e7d92a223445146c596
Melisa Çalışkan <melisaclskn5461@gmail.com>
2022-10-05 13:29:58 +0300

    update unit detail page

--- src/pages/UnitDetail/UnitDetail.js
+++ src/pages/UnitDetail/UnitDetail.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function UnitDetail() {
+  const { units } = useSelector((state) => state.units);
+  const { id } = useParams();
+  const singleUnit = units.units.find((item) => item.id === Number(id));
+  console.log(singleUnit, "single");
   return (
-    <div>UnitDetail</div>
-  )
+    <div className="container">
+      <table className="table table-striped">
+        <tbody>
+          <tr>
+            <th scope="col">id</th>
+            <td>{singleUnit.id}</td>
+          </tr>
+          <tr>
+            <th scope="col">Name</th>
+            <td>{singleUnit.name}</td>
+          </tr>
+          <tr>
+            <th scope="col">Description</th>
+            <td>{singleUnit.description}</td>
+          </tr>
+          <tr>
+            <th scope="col">Build Time</th>
+            <td>{singleUnit.build_time}</td>
+          </tr>
+          <tr>
+            <th scope="col">Reload Time</th>
+            <td>{singleUnit.reload_time}</td>
+          </tr>
+          <tr>
+            <th scope="col">Hit Points</th>
+            <td>{singleUnit.hit_points}</td>
+          </tr>
+          <tr>
+            <th scope="col">Attack</th>
+            <td>{singleUnit.attack}</td>
+          </tr>
+          <tr>
+            <th scope="col">Accuracy</th>
+            <td>{singleUnit.accuracy}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default UnitDetail
+export default UnitDetail;
